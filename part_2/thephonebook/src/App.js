8423dde5411839c4b09192e77855761a07e904c6
Lorenzo <lorenzozar@gmail.com>
2020-10-09 10:57:28 +0300

    "Add name to phonebook - Part 1 completed"

--- part_2/thephonebook/src/App.js
+++ part_2/thephonebook/src/App.js
@@ -1,24 +1,52 @@
 import React, { useState } from 'react'
+import DisplayContact from "./components/DisplayContact"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      id: 'Artooooo'
+    }
   ])
   const [newName, setNewName] = useState('')
+
+  const addContact = event => {
+    // prevents the default action of submitting a form, not in input
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const contactObj = {
+      name: newName,
+      id: newName
+    }
+    setNewName('')
+    setPersons(persons.concat(contactObj))
+  }
+
+  // called every time a change occurs in the input element
+  const handleNewName = event => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addContact}>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleNewName}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <DisplayContact key={person.id} person={person} />
+      )}
+      <div>debug: {newName}</div>
     </div>
   )
 }
---
+++ part_2/thephonebook/src/components/DisplayContact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayContact = ({ person }) => {
+    return (
+        <div>
+            {person.name}
+        </div>
+    );
+};
+
+export default DisplayContact;
