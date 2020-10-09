04d25e8455ed55abf8766d2f69d98748f4c2bcef
Lorenzo <lorenzozar@gmail.com>
2020-10-09 14:47:59 +0300

    "Fetch initial state from db.json - Step6 OK "

--- part_2/thephonebook/src/App.js
+++ part_2/thephonebook/src/App.js
@@ -1,18 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import DisplayContact from "./components/DisplayContact"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showFilter, setShowFilter] = useState('')
+
+  // Here is a promise
+  // const promise = axios.get('http://localhost:3001/persons')
+  // console.log(promise)
+  // promise.then(response => console.log(response.data))
+
+  // Using axios, update persons state fetching data from db.json
+  // axios
+  //   .get('http://localhost:3001/persons')
+  //   .then(response =>
+  //     setPersons(response.data)
+  //   )
+
+  // Using useEffect, update persons state fetching data from db.json
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  // What happens in useEffect?
+  // 1) the body of the function defining the component is executed 
+  // and the component is rendered for the first time, therefore empty (persons.length = 0).
+  // 2) the inner callback function is executed immediately after rendering
+  // it prints 'effect' and initiate the fetching of data from the server (through axios.get)
+  // it registers the next function (after .then) as an event handler
+  // 3) When data arrives, JS calls the function registered as the event handler, 
+  // which prints 'promise fulfilled'
+  // and stores the data from the server into the state using setPersons(response.data)
+  // a call to a state-updating function triggers the re-rendering of the component (persons.length = 4).
+  console.log('render', persons.length, 'contacts')
+  console.log('Indeed!')
 
 
   const addContact = event => {
@@ -58,6 +89,8 @@ const App = () => {
   const contactsToShow = showFilter
     ? persons.filter(contact => contact.name.toLowerCase().includes(showFilter.toLowerCase()))
     : persons
+
+
 
   return (
     <div>
