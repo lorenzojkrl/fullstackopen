fbd6fa48694f2c06bf88c65fa99c2eca3e6997e4
Lorenzo <lorenzozar@gmail.com>
2020-10-19 19:12:04 +0300

    frontend notes

---
+++ part_2/comments_page/build/precache-manifest.7b5888f8b9642436a1819ec65924e71f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a072bcdd4f1098b94b426e8036e6d3a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43e0182a80b17dd0539",
+    "url": "/static/css/main.fb36786a.chunk.css"
+  },
+  {
+    "revision": "090c4bafa25f021a71bc",
+    "url": "/static/js/2.a418f6ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a418f6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43e0182a80b17dd0539",
+    "url": "/static/js/main.3aa82a5b.chunk.js"
+  },
+  {
+    "revision": "c3766f920112370f5206",
+    "url": "/static/js/runtime-main.27c3a9a2.js"
+  }
+]);
