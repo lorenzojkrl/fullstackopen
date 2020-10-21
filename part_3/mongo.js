27dd929533b5f45afe55268386280c79230cc21a
Lorenzo! <lorenzozar@gmail.com>
2020-10-21 15:49:48 +0300

    MongoDB connection OK

---
+++ part_3/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+// Create personSchema Schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String // because it can contain non numeric characters
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+// Ask user to input 3 args: password, name, number
+if (process.argv.length < 5 && process.argv.length > 3) {
+    console.log(`Please provide more arguments. You are missing either the password, the name or the phone number: node mongo.js <password> <name> <number>`)
+    process.exit(1)
+} else if (process.argv.length === 3) {
+    // Get args from user
+    const password = process.argv[2]
+
+    const url =
+        `mongodb+srv://thephonebook:${password}@cluster0.1uzb3.mongodb.net/phonebookDB?retryWrites=true&w=majority`
+
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+        console.log('Connection closed')
+    })
+} else {
+    // Get args from user
+    const password = process.argv[2]
+    const nameInput = process.argv[3]
+    const numberInput = process.argv[4]
+
+    const url =
+        `mongodb+srv://thephonebook:${password}@cluster0.1uzb3.mongodb.net/phonebookDB?retryWrites=true&w=majority`
+
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+
+    const person = new Person({
+        name: nameInput,
+        number: numberInput
+    })
+
+    person.save().then(result => {
+        console.log(`added ${nameInput} number ${numberInput} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
