52cbd436ce77c95d4df5b80fcfb037c935dbd528
Lorenzo <lorenzozar@gmail.com>
2020-11-10 19:11:45 +0200

    POST fixed

--- part_4/part3-notes-backend/test/note_api.test.js
+++ part_4/part3-notes-backend/test/note_api.test.js
@@ -156,14 +156,15 @@ describe('Deletion of a note', () => {
 
 describe('when there is initially one user in db', () => {
 
-    beforeEach(async () => {
-        await User.deleteMany({})
+    // POST with Postman fail if uncomment?
+    // beforeEach(async () => {
+    //     await User.deleteMany({})
 
-        const passwordHash = await bcrypt.hash('sekret', 10)
-        const user = new User({ username: 'root', passwordHash })
+    //     const passwordHash = await bcrypt.hash('sekret', 10)
+    //     const user = new User({ username: 'root', passwordHash })
 
-        await user.save()
-    })
+    //     await user.save()
+    // })
 
     test('creation succeeds with a fresh username', async () => {
         const usersAtStart = await helper.usersInDb()
