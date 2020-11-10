054086681ffbc353c70edff30cf33da2d0f278ff
Lorenzo <lorenzozar@gmail.com>
2020-11-10 09:02:40 +0200

    Test users

--- part_4/part3-notes-backend/test/note_api.test.js
+++ part_4/part3-notes-backend/test/note_api.test.js
@@ -3,8 +3,10 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcrypt')
 
 const Note = require('../models/note')
+const User = require('../models/user')
 
 
 beforeEach(async () => {
@@ -152,6 +154,38 @@ describe('Deletion of a note', () => {
     })
 })
 
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+})
 
 afterAll(() => {
     mongoose.connection.close()
