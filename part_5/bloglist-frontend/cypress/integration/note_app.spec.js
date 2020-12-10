bdf40bc0ad93c50650d0880e14765f6ab137d82a
Lorenzo <lorenzozar@gmail.com>
2020-12-10 17:50:05 +0200

    Install Cypress

---
+++ part_5/bloglist-frontend/cypress/integration/note_app.spec.js
@@ -0,0 +1,17 @@
+describe('Blog app', function () {
+    beforeEach(function () {
+        // cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        cy.visit('http://localhost:3000')
+    })
+
+    it('Login form is shown', function () {
+        cy.contains('blogs')
+        cy.contains('Blogs, Department of Computer Science, University of Helsinki 2020')
+    })
+
+    it('user can login', function () {
+        cy.contains('login').click()
+        cy.get('input:first').type('mluukkai')
+        cy.get('input:last').type('salainen')
+    })
+})
