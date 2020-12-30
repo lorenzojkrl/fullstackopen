08f7f8889f7be3e820d978f5841477ec1b4aa0a0
Lorenzo <lorenzozar@gmail.com>
2020-12-30 14:47:01 +0200

    defining Redux-store into store.js

---
+++ part_6/redux-anecdotes/src/store.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+
+const store = createStore(
+    anecdoteReducer,
+    composeWithDevTools()
+)
+
+export default store;
