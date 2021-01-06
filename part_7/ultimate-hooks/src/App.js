e0b7129812b47e2ba95b0255399d6d5aa927660a
Lorenzo <lorenzozar@gmail.com>
2021-01-06 10:30:55 +0200

    implement useResource

--- part_7/ultimate-hooks/src/App.js
+++ part_7/ultimate-hooks/src/App.js
@@ -1,4 +1,4 @@
-  
+
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
@@ -20,18 +20,33 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  // useEffect moved into App to fetch the first list asap
+  // useEffect(() => {
+  //   axios
+  //     .get(baseUrl)
+  //     .then((response) => {
+  //       setResources(response.data)
+  //     })
+  // }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
+
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
   }
 
   const service = {
-    create
+    create, getAll
   }
 
   return [
-    resources, service
+    resources,
+    service
   ]
 }
 
@@ -43,14 +58,20 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
+  useEffect(() => {
+    noteService.getAll()
+    personService.getAll()
+    // console.log('useEffect');
+  }, [])
+
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -64,7 +85,7 @@ const App = () => {
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
