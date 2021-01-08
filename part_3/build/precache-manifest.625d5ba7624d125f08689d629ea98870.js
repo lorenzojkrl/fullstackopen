70c86de31c10ed45e90a0656f3c860668d83b175
Lorenzo <lorenzozar@gmail.com>
2021-01-08 19:56:28 +0200

    phonebook backend

---
+++ part_3/build/precache-manifest.625d5ba7624d125f08689d629ea98870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5d1a9d4480805b4e0aef4e0451f181",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45dfadf2ec81836c17aa",
+    "url": "/static/css/main.6808956c.chunk.css"
+  },
+  {
+    "revision": "574b33d54b7d55d467ae",
+    "url": "/static/js/2.3976bcf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3976bcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45dfadf2ec81836c17aa",
+    "url": "/static/js/main.119557e2.chunk.js"
+  },
+  {
+    "revision": "c771fc46209c33cf411a",
+    "url": "/static/js/runtime-main.3a2acdf3.js"
+  }
+]);
