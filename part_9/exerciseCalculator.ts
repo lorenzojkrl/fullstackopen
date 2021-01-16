7949a020ab1d501dd8a3b454cfec74bfe28ff344
Lorenzo <lorenzozar@gmail.com>
2021-01-16 17:13:24 +0200

    bmiCalculator accepts command line args

--- part_9/exerciseCalculator.ts
+++ part_9/exerciseCalculator.ts
@@ -1,3 +1,42 @@
-const calculateExercises = () => {
+interface Metrics {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+let sampleArray = [3, 0, 2, 4.5, 0, 3, 1]
+const calculateExercises = (dailyHours: Array<number>, targetHours: number) : Metrics => {
+  let trainingDays: number = dailyHours.filter(num => num !== 0).length
+  let avg = dailyHours.reduce((a, b) => a + b, 0)/dailyHours.length
+
+  let isSuccessful: boolean = true
+  let rating = 3
+  let ratingDescription = 'You made it! You achieved your target!'
+  if(avg<targetHours) {
+    isSuccessful = false
+    if(avg > .8*targetHours){
+      rating = 2
+      ratingDescription = 'You almost made it! Good Effort!'
+    }else{
+      rating = 1
+      ratingDescription = 'It seems you weren\'t to focused this time'
+    }
+  }
+
+  return {
+    periodLength : dailyHours.length,
+    trainingDays: trainingDays,
+    success: isSuccessful,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: targetHours,
+    average: avg
+  } 
 
 }
+
+console.log(calculateExercises(sampleArray, 2));
