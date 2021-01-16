1f50417d299f96dc2a6875a2d379611e58bca26f
Lorenzo <lorenzozar@gmail.com>
2021-01-16 13:23:17 +0200

    bmiCalculator

--- part_9/multiplier.ts
+++ part_9/multiplier.ts
@@ -1,28 +1,29 @@
-// We can create a type using the TypeScript native keyword type. 
-// Using the OR operator | we can define a variable to accept multiple values by creating a union type. 
-// In this case we used exact strings (that in technical terms are called string literal types) 
-// but with unions you could also make the compiler to accept for example both string and number: string | number.
-// The type keyword defines a new name for a type, a type alias. 
-// Since the defined type is a union of three possible values, 
-// it is handy to give it an alias that has a representative name.
-type Operation = 'multiply' | 'add' | 'divide';
-type Result = number
-const calculator = (a: number, b: number, op: Operation): Result => {
-    switch(op){
-        case 'multiply':
-            return a*b;
-    case 'add':
-        return a+b;
-    case 'divide':
-        if(b===0) throw new Error ('Can\'t divide by 0!');
-        return a/b;
-    default:
-        throw new Error('Operation is not multiply, add or divide!');
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: Array<string>): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
     }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
-  
-  try {
-    console.log(calculator(1, 5 , 'divide'))
-  } catch (e) {
-    console.log('Something went wrong, error message: ', e.message);
-  }
+}
+
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText,  a * b);
+}
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
