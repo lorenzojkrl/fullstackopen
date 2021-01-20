caba717b76ce5fb807164bfe6a5a7be15d5222a6
Lorenzo <lorenzozar@gmail.com>
2021-01-20 15:53:25 +0200

    convert json to ts

--- part_9/Ilari/src/services/diaryService.ts
+++ part_9/Ilari/src/services/diaryService.ts
@@ -1,8 +1,9 @@
-import diaryData from "../../data/diaries.json";
+import diaries from "../../data/diaries";
 import { DiaryEntry } from "../types";
 
 // Type assertion
-const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
+// const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
+// Better to convert diaries.json to ts and export typed diaries
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
--- part_9/Ilari/src/types.ts
+++ part_9/Ilari/src/types.ts
@@ -1,9 +1,13 @@
 export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy";
 export type Visibility = "great" | "good" | "ok" | "poor";
+
+// if we want to be able to save entries without a certain field,
+// e.g. comment,
+// we could set the type of the field as optional by adding ? to the type declaration:
 export interface DiaryEntry {
   id: number;
   date: string;
   weather: Weather;
   visibility: Visibility;
-  comment: string;
+  comment?: string;
 }
