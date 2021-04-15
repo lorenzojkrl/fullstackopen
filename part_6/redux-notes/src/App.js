113926a28b6abe87acdf90e89681428a09582d51
Lorenzo <lorenzozar@gmail.com>
2021-04-15 10:33:40 +0300

    refactor note creation into NewNote

--- part_6/redux-notes/src/App.js
+++ part_6/redux-notes/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import { toggleImportanceOf } from './reducers/noteReducer'
 import { useSelector, useDispatch } from 'react-redux'
-
+import NewNote from './NewNote'
 
 const App = () => {
   const dispatch = useDispatch()
   const notes = useSelector(state => state)
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    dispatch(createNote(content))
-  }
 
   const toggleImportance = (id) => {
     dispatch(toggleImportanceOf(id))
@@ -20,10 +13,7 @@ const App = () => {
 
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
+      <NewNote />
       <ul>
         {notes.map(note =>
           <li
