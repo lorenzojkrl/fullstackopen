c404228de2b101237a14317aee3586df531647be
Lorenzo <lorenzozar@gmail.com>
2021-04-15 10:36:18 +0300

    refactor notes list into Notes

--- part_6/redux-notes/src/App.js
+++ part_6/redux-notes/src/App.js
@@ -1,29 +1,13 @@
 import React from 'react'
-import { toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
 import NewNote from './NewNote'
+import Notes from './Notes'
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
 
   return (
     <div>
       <NewNote />
-      <ul>
-        {notes.map(note =>
-          <li
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <Notes />
     </div>
   )
 }
---
+++ part_6/redux-notes/src/Notes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { toggleImportanceOf } from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
+
+
+
+const Notes = () => {
+  const notes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
+
+  return (
+    <ul>
+      {notes.map(note =>
+        <li
+          key={note.id}
+          onClick={() => toggleImportance(note.id)}
+        >
+          {note.content} <strong>{note.important ? 'important' : ''}</strong>
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Notes;
