780247c160210149dbc6da697947cc914aa0d501
Lorenzo <lorenzozar@gmail.com>
2021-04-15 11:04:01 +0300

    reducer & action creator to save votes

--- part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,25 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      let newState = state.map(anecdote => {
+        if (anecdote.id === action.payload) {
+          anecdote.votes += 1
+        }
+        return anecdote
+      })
+      return newState
+    default:
+      return state
+  }
+}
+
+export const voteAnecdote = id => {
+  return {
+    type: 'VOTE',
+    payload: id
+  }
 }
 
 export default reducer
