e637126c68ac44670021dd2ed5ed9ec26b5781d4
Lorenzo <lorenzozar@gmail.com>
2021-04-21 12:01:08 +0300

    redux devtools setup

--- part_6/redux-anecdotes/src/index.js
+++ part_6/redux-anecdotes/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+)
 
 ReactDOM.render(
   <Provider store={store}>
