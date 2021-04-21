fae8fb80af5124bc127aa9c7910e371e00dfb37c
Lorenzo <lorenzozar@gmail.com>
2021-04-21 12:16:38 +0300

    move Components into /components

--- part_6/redux-anecdotes/src/App.js
+++ part_6/redux-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import AnecdoteForm from './AnecdoteForm'
-import AnecdoteList from './AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 const App = () => {
   return (
     <div>
