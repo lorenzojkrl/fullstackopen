a06f869b8a1b68992ecb4ead5510188d49af568e
Lorenzo <lorenzozar@gmail.com>
2021-04-23 10:44:09 +0300

    refactor store & use thunk

--- part_6/redux-anecdotes/src/index.js
+++ part_6/redux-anecdotes/src/index.js
@@ -1,25 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import store from './store'
+
 import App from './App'
-
-import anecdoteReducer from './reducers/anecdoteReducer'
-import notificationReducer from './reducers/notificationReducer'
-import filterReducer from './reducers/filterReducer'
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
-})
-
-const store = createStore(
-  reducer,
-  composeWithDevTools()
-)
 
 ReactDOM.render(
   <Provider store={store}>
