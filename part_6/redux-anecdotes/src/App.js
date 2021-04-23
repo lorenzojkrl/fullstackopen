cc3b308af97c30e5baee6d6e7a23760b7d789764
Lorenzo <lorenzozar@gmail.com>
2021-04-23 10:50:41 +0300

    init redux-store uses asynchronous action creator

--- part_6/redux-anecdotes/src/App.js
+++ part_6/redux-anecdotes/src/App.js
@@ -5,7 +5,6 @@ import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 
-import anecdoteService from './services/anecdotes'
 import { useDispatch } from 'react-redux'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
@@ -13,11 +12,7 @@ const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
-      .then(anecdotes => console.log(anecdotes))
-
+    dispatch(initializeAnecdotes())
   }, [dispatch])
   return (
     <div>
--- part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,6 +1,4 @@
-// import { asObject } from './store'
-// asObject(action.payload) was used to assign an id and create an object anecdote
-// No the server assigns an id
+import anecdoteService from '../services/anecdotes'
 
 const reducer = (state = [], action) => {
   console.log('state now: ', state)
@@ -38,10 +36,13 @@ export const createAnecdote = content => {
   }
 }
 
-export const initializeAnecdotes = anecdotes => {
-  return {
-    type: 'INIT',
-    payload: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT',
+      payload: anecdotes
+    })
   }
 }
 
